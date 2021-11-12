a98374c53d7eb6ed43ca35ffccc7a7017166cbd4
saifsiddiquee <saif.rafi07@gmail.com>
2021-11-12 14:37:32 +0600

    project update

---
+++ lib/pages/profile.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:face_net_authentication/pages/widgets/app_button.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'home.dart';
+import 'dart:math' as math;
+
+class Profile extends StatelessWidget {
+  const Profile(this.username, {Key key, this.imagePath}) : super(key: key);
+  final String username;
+  final String imagePath;
+
+  final String githubURL =
+      "https://github.com/MCarlomagno/FaceRecognitionAuth/tree/master";
+
+  void _launchURL() async => await canLaunch(githubURL)
+      ? await launch(githubURL)
+      : throw 'Could not launch $githubURL';
+
+  @override
+  Widget build(BuildContext context) {
+    final double mirror = math.pi;
+    return Scaffold(
+      backgroundColor: Color(0XFFADD8E6),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: FileImage(File(imagePath)),
+                      ),
+                    ),
+                    margin: EdgeInsets.all(20),
+                    width: 50,
+                    height: 50,
+                    // child: Transform(
+                    //     alignment: Alignment.center,
+                    //     child: FittedBox(
+                    //       fit: BoxFit.cover,
+                    //       child: Image.file(File(imagePath)),
+                    //     ),
+                    //     transform: Matrix4.rotationY(mirror)),
+                  ),
+                  Text(
+                    '${greeting()}, ' + username + '!',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                /*decoration: BoxDecoration(
+                  color: Color(0xFFFEFFC1),
+                  borderRadius: BorderRadius.circular(10),
+                ),*/
+                child: Container(),
+              ),
+              Spacer(),
+              AppButton(
+                text: "LOG OUT",
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()),
+                      (Route<dynamic> route) => false);
+                },
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                color: Colors.redAccent,
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String greeting() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Good Morning';
+    }
+    if (hour < 17) {
+      return 'Good Afternoon';
+    }
+    return 'Good Evening';
+  }
+}
